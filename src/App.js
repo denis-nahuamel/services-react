174dbdd3c694df935880d7856e8425620008e70b
denis-nahuamel <nahuamel.denis@gmail.com>
2022-08-18 16:47:51 +0000

    show all available services

--- src/App.js
+++ src/App.js
@@ -3,16 +3,12 @@ import './App.css';
 import Header from './header';
 import CardService from './components/card';
 import AddService from './components/add-service';
-import { FirebaseAppProvider } from 'reactfire';
-import { firebaseConfig } from './connection/firebase-config';
+import ListAllServices from './pages/list-all-services';
 
 function App() {
   return (
-    <FirebaseAppProvider firebaseConfig={firebaseConfig}>
-      <Suspense fallback={<p>Cargando...</p>}>
-        <AddService />
-      </Suspense>
-    </FirebaseAppProvider>
+    
+        <ListAllServices />
   );
 }
 
