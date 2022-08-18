159bea4d533323bac265643d7cc2b51358ee5dad
denis-nahuamel <nahuamel.denis@gmail.com>
2022-08-18 16:09:39 +0000

    add firebase configuration

--- src/App.js
+++ src/App.js
@@ -3,10 +3,16 @@ import './App.css';
 import Header from './header';
 import CardService from './components/card';
 import AddService from './components/add-service';
+import { FirebaseAppProvider } from 'reactfire';
+import { firebaseConfig } from './connection/firebase-config';
 
 function App() {
   return (
-      <AddService />
+    <FirebaseAppProvider firebaseConfig={firebaseConfig}>
+      <Suspense fallback={<p>Cargando...</p>}>
+        <AddService />
+      </Suspense>
+    </FirebaseAppProvider>
   );
 }
 
