41c5be048e329411dcdde2a5c747ff04f5bf417c
denis-nahuamel <nahuamel.denis@gmail.com>
2022-08-18 18:44:30 +0000

    change type of service

--- src/App.js
+++ src/App.js
@@ -5,12 +5,17 @@ import CardService from './components/card';
 import AddService from './components/add-service';
 import ListAllServices from './components/list-all-services';
 import ServicesPage from './pages/services-page';
+import { useState } from 'react';
 
 function App() {
+  const [type, setType] = useState("nocategory")
+  const handleType = (value)=>{
+    setType(value)
+  }
   return (
         <div className='d-flex flex-column'>
-        <Header />
-        <ServicesPage />
+        <Header onHandleType={handleType}/>
+        <ServicesPage onSetType={type}/>
         </div>
   );
 }
--- src/pages/services-page.js
+++ src/pages/services-page.js
@@ -1,11 +1,10 @@
 import AddService from "../components/add-service";
 import ListAllServices from "../components/list-all-services";
 
-const ServicesPage = () => {
-
+const ServicesPage = ({onSetType}) => {
     return (
         <div className="d-flex flex-row gap-3 m-3 justify-content-between">
-            <ListAllServices />
+            <ListAllServices onSetType ={onSetType}/>
             <AddService />
         </div>
     )
