cb4f721b561964d39ce39870299c6cb1cd55642f
denis-nahuamel <nahuamel.denis@gmail.com>
2022-08-19 17:54:09 +0000

    add CRUD

--- src/services/firestore-service.js
+++ src/services/firestore-service.js
@@ -1,17 +1,15 @@
 import 'firebase/firestore';
 import { firebase } from '../connection/firebase';
 
-export const getServices = async (category) =>{
-    
-    console.log("getservices", category)
-    const db = firebase.firestore()
+export const getServices = async (category) =>{ 
+    const db = firebase.firestore().collection('services')
     try{
         let data = null;
         if(category !="nocategory"){
-            data = await db.collection('services').where("type", "==", category).get();
+            data = await db.where("type", "==", category).get();
         }
         else{
-            data = await db.collection('services').get()
+            data = await db.get()
         }
         const arrayData = data.docs.map(doc => ({ id: doc.id, ...doc.data() }))
         return arrayData;
@@ -20,4 +18,32 @@ export const getServices = async (category) =>{
         console.log(e)
     }
     
+}
+export const addService = async (service)=>{
+    try {
+        const db = firebase.firestore().collection('services')
+        const data = await db.add(service)
+        return data;
+    } catch (e) {
+        console.log(e)
+    }
+}
+export const updateService = async (id,service)=>{
+    try {
+        const db = firebase.firestore().collection('services')
+        const data = await db.doc(id).update(service)
+        return data;
+    } catch (e) {
+        console.log(e)
+    }
+}
+export const deleteService = async (id) =>{
+    try {
+        const db = firebase.firestore()
+        const response = await db.collection('services').doc(id).delete();
+
+        return response;
+      } catch (e) {
+        console.log(e)
+      }
 }
