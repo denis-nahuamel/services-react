fb7726040d510b119b80d02aa4ee66ff11bc02df
denis-nahuamel <nahuamel.denis@gmail.com>
2022-08-19 17:55:03 +0000

    edit and delete services

--- src/components/card.js
+++ src/components/card.js
@@ -1,7 +1,22 @@
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import { useServiceProvider } from '../context/service-context';
+import { deleteService } from '../services/firestore-service';
 const CardService = ({data}) =>{
-    const {type,name,description} = data
+  const {setReloadContext, setServiceUpdateContext} = useServiceProvider();
+    const {_type,name,description} = data;
+    const handleDelete = (event) => {
+      event.preventDefault();
+      deleteService(data.id).then(()=>setReloadContext(true));
+    }
+    const handleEdit = (event)=>{
+      event.preventDefault();
+      setServiceUpdateContext({
+        id: data.id,
+        nombre: name,
+        descripcion: description
+      })
+    }
     return (
         <Card style={{ width: '18rem', height:'10rem'}} >
           <Card.Body>
@@ -11,8 +26,8 @@ const CardService = ({data}) =>{
             </Card.Text>
           </Card.Body>
           <Card.Footer >
-             <Button variant="light">Editar</Button>
-             <Button variant="light">Eliminar</Button>
+             <Button variant="light" onClick={handleEdit}>Editar</Button>
+             <Button variant="light" onClick={handleDelete}>Eliminar</Button>
         </Card.Footer>
         </Card>
       );
